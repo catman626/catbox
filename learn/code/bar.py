f5a2e6ab9555c0e0eed22014f6fc84702e19477f
catman626 <catman626@163.com>
2026-01-31 16:22:28 +0800

    auto-checkpoint

--- learn/code/bar.py
+++ learn/code/bar.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# plt.subplot([2, 1, 1])
 
-plt.subplot([2, 1, 1])
+import matplotlib 
+matplotlib.use("agg")
 
 a = np.array([1, 2, 3, 6])
 b = np.array([5, 6, 7, 8])
 
 plt.bar(a, b)
 
-plt.show()
+# plt.show()
+plt.savefig("demo")
